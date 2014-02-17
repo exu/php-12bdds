fbb360ae9418d7fb97e1cfc751fe47c6b5079968
Jacek Wysocki <jacek.wysocki@gmail.com>
2014-02-17 22:15:03 +0100

    added some descriptions to each task

--- spec/Exc01/CalcStatsSpec.php
+++ spec/Exc01/CalcStatsSpec.php
@@ -5,6 +5,23 @@ namespace spec\Exc01;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+
+/**
+ *  Your task is to process a sequence of integer numbers
+ *  to determine the following statistics:
+ *
+ *  o) minimum value
+ *  o) maximum value
+ *  o) number of elements in the sequence
+ *  o) average value
+ *
+ *  For example: [6, 9, 15, -2, 92, 11]
+ *
+ *  o) minimum value = -2
+ *  o) maximum value = 92
+ *  o) number of elements in the sequence = 6
+ *  o) average value = 21.833333
+ */
 class CalcStatsSpec extends ObjectBehavior
 {
     protected $data = [3,3,4,5,6,99,-99,7,8,64];
--- spec/Exc02/NumberNamesSpec.php
+++ spec/Exc02/NumberNamesSpec.php
@@ -5,6 +5,20 @@ namespace spec\Exc02;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ *  Spell out a number. For example
+ *
+ *  99 –> ninety nine
+ *  300 –> three hundred
+ *  310 –> three hundred and ten
+ *  1501 –> one thousand, five hundred and one
+ *  12609 –> twelve thousand, six hundred and nine
+ *  512607 –> five hundred and twelve thousand,
+ *  six hundred and seven
+ *  43112603 –> forty three million, one hundred and
+ *  twelve thousand,
+ *  six hundred and three
+ */
 class NumberNamesSpec extends ObjectBehavior
 {
     protected $data = [
